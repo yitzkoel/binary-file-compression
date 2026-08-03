bef8da37746c8776b6974b95e737429604ad304b
yitzkoel <yitzkoel@gmail.com>
2026-08-03 17:06:09 +0300

    created the class FileReader that provides abstraction of reading from a file, for the lempel ziv77 algo. o Please enter the commit message for your changes. Lines starting

---
+++ cpp/src/binary_io.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yitzk on 8/3/2026.
+//
+
+#include "binary_io.h"
---
+++ cpp/src/binary_io.h
@@ -0,0 +1,16 @@
+//
+// Created by yitzk on 8/3/2026.
+//
+
+#ifndef BINARY_IO_H
+#define BINARY_IO_H
+
+
+
+class binary_io {
+
+};
+
+
+
+#endif //BINARY_IO_H
